bfacad1df8d46e403df7ade00ebeb496bf3f37f4
akasharvan <akasharvan@gmail.com>
2025-03-22 13:39:16 +0530

    Update dice rolling game.py

--- dice rolling game.py
+++ dice rolling game.py
@@ -1,25 +1,23 @@
-# loop
-# Ask user to roll the dice?.lower is to convert user choice in lower case..
-#  If user enter y?
-#   generate 2 random numbers
-#   and print.
-# If user enter n?
-#  print thank You message
-#  terminate
-# else
-#  print invalid choice
-
 import random 
 
-  
+# loop  
 while True:
+    # Ask user to roll the dice?.lower is to convert user choice in lower case.
     choice = input("Roll the Dice? (y/n): ").lower()
+    #  If user enter y?
     if choice == 'y':
+        #   generate 2 random numbers
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
+        #   and print.
         print(f'({die1},{die2})')
+    # If user enter n?
     elif choice == 'n':
+        #  print thank You message
         print("Thanks for playing")
+        #  terminate
         break
+    # else
     else:
+        #  print invalid choice
         print("INVALID CHOICE")
