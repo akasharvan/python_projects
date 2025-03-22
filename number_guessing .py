c52382696e5b0a7b69b8cc14bc1aa1af2957e9b8
akasharvan <akasharvan@gmail.com>
2025-03-22 13:49:11 +0530

    Update number_guessing .py

--- number_guessing .py
+++ number_guessing .py
@@ -1,30 +1,26 @@
-# Generate a random number
-# loop
-    # Ask user to make a guess?
-    # if not a valid number 
-    #   Print an error
-    # if number < guess
-    #  print too low
-    # If number > gusee
-    #  print too high
-    # else
-    #  print well done
-
-
-
 import random
 
+# Generate a random number
 num_to_guess = random.randint(1,100)
+# loop
 while True:
     try:
+        # Ask user to make a guess?
         guess = int(input('Guess the number b/w (1 to 100): '))
+        # if number < guess
         if (guess < num_to_guess):
+            #  print too low
             print('your guess is too low')
+        # If number > gusee
         elif(guess > num_to_guess):
+            #  print too high
             print('your guess is too High')
+        # else
         else:
+            #  print well done
             print ('congratulations! You guess the number')
             break
     except ValueError:
+        #   Print an error
         print('Please enter a valid number')
 
